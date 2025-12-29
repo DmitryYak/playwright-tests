10f9418b7864bdadad40a2bce9d56b7b6ceff329
Dmitriy <yakovlev.drezna@gmail.com>
2025-12-29 22:08:18 +0300

    add tests saucedemo

--- tests/shop_demo/login.spec.ts
+++ tests/shop_demo/login.spec.ts
@@ -5,14 +5,33 @@ const urlMain = "https://www.saucedemo.com/";
 test.describe("SauceDemo Login", () => {
   test.beforeEach(async ({ page }) => {
     await page.goto(urlMain);
-    await expect(page.locator(".login_logo")).toBeVisible(); // ← ТОЧКА!
+    await expect(page.locator(".login_logo")).toBeVisible();
   });
 
-  test("positive login", async ({ page }) => {
+  test(`check login positive`, async ({ page }) => {
     await page.locator('[data-test="username"]').fill("standard_user");
     await page.locator('[data-test="password"]').fill("secret_sauce");
-    await page.locator('[data-test="login-button"]').click();
 
-    await expect(page).toHaveURL(/inventory/);
+    await page.locator('[data-test="login-button"]').click();
+    await expect(page).toHaveURL("https://www.saucedemo.com/inventory.html");
+  });
+  test("check logo", async ({ page }) => {
+    await expect(page.getByText("Swag Labs")).toBeVisible();
+  });
+
+  test(`check button menu, have buttons`, async ({ page }) => {
+    await page.getByRole("button", { name: "Open Menu" }).click();
+    await expect(
+      page.locator('[data-test="inventory-sidebar-link"]')
+    ).toBeVisible();
+    await expect(
+      page.locator('[data-test="about-sidebar-link"]')
+    ).toBeVisible();
+    await expect(
+      page.locator('[data-test="logout-sidebar-link"]')
+    ).toBeVisible();
+    await expect(
+      page.locator('[data-test="reset-sidebar-link"]')
+    ).toBeVisible();
   });
 });
